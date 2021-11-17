0796c8ed7367d1b643a7073f5e8eff7a192db40a
niuben <ben.niu@infiray.com>
2021-11-17 15:35:09 +0800

    modfify the pixel to 1920*1080

--- Makefile
+++ Makefile
@@ -8,7 +8,9 @@ CROSS=/home/haitu/newcode/Cx_rv1126_v1.8.0/prebuilts/gcc/linux-x86/arm/gcc-arm-8
 
 RKNN_API_DIR=$(SDK_TOP_DIR)external/rknpu/rknn/rknn_api
 RKNN_DEMO_DIR=$(RKNN_API_DIR)/examples
-
+JPEG_DIR=/home/haitu/newcode/Cx_rv1126_v1.8.0/external/minigui/src/image/
+#JPEG_DIR=/home/haitu/RV1126_RV1109_LINUX_SDK_V2.1.0_20210512/buildroot/output/rockchip_rv11xx_rtcore_38ipc/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include
+HIG_DIR=/home/haitu/newcode/Cx_rv1126_v1.8.0/external/rkmedia/examples/3rdparty/opencv/opencv-linux-armhf/include/opencv/
 CFLAGS=-I include \
        -I $(RKNN_DEMO_DIR)/3rdparty/stb \
        -I $(RKNN_DEMO_DIR)/3rdparty/ \
@@ -17,6 +19,8 @@ CFLAGS=-I include \
        -I $(RKNN_DEMO_DIR)/3rdparty/drm/include/libdrm \
        -I $(RKNN_DEMO_DIR)/3rdparty/opencv/opencv-linux-armhf/include \
        -I $(RKNN_API_DIR)/librknn_api/include \
+       -I $(JPEG_DIR) \
+       -I $(HIG_DIR) \
 
 LDFLAGS=-ldl $(RKNN_API_DIR)/librknn_api/lib/librknn_api.so
 
--- main.cpp
+++ main.cpp
@@ -12,24 +12,77 @@
 
 //#include <opencv2/highgui.hpp>
 #include "RkNNImageFusion.h"
+//#include "highgui.h"
+
+extern "C" {
+#include <jpeglib.h>
+}
+
+#define JPEG_QUALITY 100 //图片质量
+
 
 using namespace std;
 using namespace cv;
 
 #define IMG_COUNT  1//133		//我们有133张图
 
-//#define IMG_WIDTH  (256*2)
-//#define IMG_HEIGHT (192*2)
+#define IMG_WIDTH  (256*2)
+#define IMG_HEIGHT (192*2)
+//#define JPEG_LIB_VERSION 62
 
 
-#define IMG_WIDTH  1920
-#define IMG_HEIGHT 1080
+#define IMG_WIDTH1920  1920
+#define IMG_HEIGHT1080 1080
 
 
 char szVisData[IMG_WIDTH * IMG_HEIGHT * 3];
 char szInfData[IMG_WIDTH * IMG_HEIGHT * 3];
 char szFusionData[IMG_WIDTH * IMG_HEIGHT * 3];
 
+
+#if 1
+int savejpg(uchar *pdata, char *jpg_file, int width, int height)
+{  //分别为RGB数据，要保存的jpg文件名，图片长宽
+    int depth = 3;
+    JSAMPROW row_pointer[1];//指向一行图像数据的指针
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    FILE *outfile;
+ 
+    cinfo.err = jpeg_std_error(&jerr);//要首先初始化错误信息
+    //* Now we can initialize the JPEG compression object.
+    jpeg_create_compress(&cinfo);
+ 
+    if ((outfile = fopen(jpg_file, "wb")) == NULL)
+    {
+        fprintf(stderr, "can't open %s\n", jpg_file);
+        return -1;
+    }
+    jpeg_stdio_dest(&cinfo, outfile);
+ 
+    cinfo.image_width = width;             //* image width and height, in pixels
+    cinfo.image_height = height;
+    cinfo.input_components = depth;    //* # of color components per pixel
+    cinfo.in_color_space = JCS_RGB;     //* colorspace of input image
+    jpeg_set_defaults(&cinfo);
+ 
+    jpeg_set_quality(&cinfo, JPEG_QUALITY, TRUE ); //* limit to baseline-JPEG values
+    jpeg_start_compress(&cinfo, TRUE);
+ 
+    int row_stride = width * 3;
+    while (cinfo.next_scanline < cinfo.image_height)
+           {
+            row_pointer[0] = (JSAMPROW)(pdata + cinfo.next_scanline * row_stride);//一行一行数据的传，jpeg为大端数据格式
+            jpeg_write_scanlines(&cinfo, row_pointer, 1);
+        }
+ 
+    jpeg_finish_compress(&cinfo);
+    jpeg_destroy_compress(&cinfo);//这几个函数都是固定流程
+    fclose(outfile);
+ 
+    return 0;
+}
+#endif
 int ReadFile(const char *pszName, char *pszData, int iLen)
 {
     int fd = -1;
@@ -85,21 +138,21 @@ int main(int argc, char *argv[])
 		ReadFile(szRgbFileName, szVisData, sizeof(szVisData));
 
         
-//        cv::Mat matvis(IMG_WIDTH, IMG_HEIGHT, CV_8UC3, szVisData);
-////        cvtColor(matvis, matBgr, COLOR_RGB2BGR);
-////        std::vector<Mat> mv_vis;
-////        split(matvis, (vector<Mat>&)mv_vis);
-////        imagevis = mv_vis[0].clone();
-//        cv::imwrite("./img0.jpg", matvis);
 
-        Mat image2YUV;
+       
+       
+        
+        Mat image2BGR(Size(IMG_WIDTH, IMG_HEIGHT), CV_8UC3);
         Mat matvis(Size(IMG_WIDTH, IMG_HEIGHT), CV_8UC3, szVisData);
-		cvtColor(matvis, image2YUV, CV_RGB2YUV);
+		cvtColor(matvis, image2BGR, COLOR_RGB2BGR);
 		//video << matBgr;
-
+        
 		//sprintf(szRgbFileName, "fus_%d.jpg", 0);
-		cv::imwrite("./img0.jpg", matvis);
+		cv::imwrite("./img0.jpg", image2BGR); 
 
+        cv::Mat img = image2BGR.clone();
+        cv::resize(image2BGR, img, cv::Size(IMG_WIDTH1920, IMG_HEIGHT1080), cv::INTER_LINEAR);
+        imwrite("result2.jpg", img); 
 
         return 0;
         
