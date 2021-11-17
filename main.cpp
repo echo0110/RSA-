25a7b95f2a80d2cb0d1b2cd36d974658169cd528
niuben <ben.niu@infiray.com>
2021-11-17 15:54:30 +0800

    modfify the pixel to 1920*1080  inf and vis

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ extern "C" {
 using namespace std;
 using namespace cv;
 
-#define IMG_COUNT  1//133		//我们有133张图
+#define IMG_COUNT  3//133		//我们有133张图
 
 #define IMG_WIDTH  (256*2)
 #define IMG_HEIGHT (192*2)
@@ -40,49 +40,7 @@ char szInfData[IMG_WIDTH * IMG_HEIGHT * 3];
 char szFusionData[IMG_WIDTH * IMG_HEIGHT * 3];
 
 
-#if 1
-int savejpg(uchar *pdata, char *jpg_file, int width, int height)
-{  //分别为RGB数据，要保存的jpg文件名，图片长宽
-    int depth = 3;
-    JSAMPROW row_pointer[1];//指向一行图像数据的指针
-    struct jpeg_compress_struct cinfo;
-    struct jpeg_error_mgr jerr;
-    FILE *outfile;
- 
-    cinfo.err = jpeg_std_error(&jerr);//要首先初始化错误信息
-    //* Now we can initialize the JPEG compression object.
-    jpeg_create_compress(&cinfo);
- 
-    if ((outfile = fopen(jpg_file, "wb")) == NULL)
-    {
-        fprintf(stderr, "can't open %s\n", jpg_file);
-        return -1;
-    }
-    jpeg_stdio_dest(&cinfo, outfile);
- 
-    cinfo.image_width = width;             //* image width and height, in pixels
-    cinfo.image_height = height;
-    cinfo.input_components = depth;    //* # of color components per pixel
-    cinfo.in_color_space = JCS_RGB;     //* colorspace of input image
-    jpeg_set_defaults(&cinfo);
- 
-    jpeg_set_quality(&cinfo, JPEG_QUALITY, TRUE ); //* limit to baseline-JPEG values
-    jpeg_start_compress(&cinfo, TRUE);
- 
-    int row_stride = width * 3;
-    while (cinfo.next_scanline < cinfo.image_height)
-           {
-            row_pointer[0] = (JSAMPROW)(pdata + cinfo.next_scanline * row_stride);//一行一行数据的传，jpeg为大端数据格式
-            jpeg_write_scanlines(&cinfo, row_pointer, 1);
-        }
- 
-    jpeg_finish_compress(&cinfo);
-    jpeg_destroy_compress(&cinfo);//这几个函数都是固定流程
-    fclose(outfile);
- 
-    return 0;
-}
-#endif
+
 int ReadFile(const char *pszName, char *pszData, int iLen)
 {
     int fd = -1;
@@ -129,39 +87,39 @@ int main(int argc, char *argv[])
 
 #if 1
 	gettimeofday(&start_time, NULL);
-	for (int i = 0; i < IMG_COUNT; i++)
+	for (int i = 2; i < IMG_COUNT; i++)
 	{
 		//分别读取红外和可见光图像
 		sprintf(szRgbFileName, "res/inf_%d.rgb", i);
+        printf(" %s,%d, szRgbFileName is %s\n",__func__,__LINE__,szRgbFileName);
 		ReadFile(szRgbFileName, szInfData, sizeof(szInfData));
-		sprintf(visFileName, "res/vis_%d.rgb", i);
+		sprintf(szRgbFileName, "res/vis_%d.rgb", i);
+        printf(" %s,%d, szRgbFileName is %s\n",__func__,__LINE__,szRgbFileName);
 		ReadFile(szRgbFileName, szVisData, sizeof(szVisData));
 
         
-
+       
        
        
         
         Mat image2BGR(Size(IMG_WIDTH, IMG_HEIGHT), CV_8UC3);
         Mat matvis(Size(IMG_WIDTH, IMG_HEIGHT), CV_8UC3, szVisData);
 		cvtColor(matvis, image2BGR, COLOR_RGB2BGR);
-		//video << matBgr;
-        
-		//sprintf(szRgbFileName, "fus_%d.jpg", 0);
 		cv::imwrite("./img0.jpg", image2BGR); 
-
         cv::Mat img = image2BGR.clone();
         cv::resize(image2BGR, img, cv::Size(IMG_WIDTH1920, IMG_HEIGHT1080), cv::INTER_LINEAR);
-        imwrite("result2.jpg", img); 
+        imwrite("vis_0.jpg", img); 
 
-        return 0;
+
+        Mat image3BGR( Size(IMG_WIDTH, IMG_HEIGHT), CV_8UC3);
+        Mat matinf(Size(IMG_WIDTH, IMG_HEIGHT), CV_8UC3, szInfData);
+        cvtColor(matinf, image3BGR, COLOR_RGB2BGR);
+        cv::imwrite("./img1.jpg", image3BGR); 
+        cv::Mat img2 = image3BGR.clone();
+        cv::resize(image3BGR, img2, cv::Size(IMG_WIDTH1920, IMG_HEIGHT1080), cv::INTER_LINEAR);
+        imwrite("inf_0.jpg", img2); 
         
-        Mat matinf( Size(IMG_WIDTH, IMG_HEIGHT), CV_8UC3, szInfData);
-        cvtColor(matinf, matBgr, COLOR_RGB2BGR);
-        std::vector<Mat> mv_inf;
-        split(matinf, (vector<Mat>&)mv_inf);
-        //imageinf = mv_inf[0].clone();
-        cv::imwrite("./img1.jpg", matinf);
+        
 		//执行融合
 		RKNN_ImgFusionProcess(imagevis.data, imageinf.data, szFusionData, IMG_WIDTH, IMG_HEIGHT,matinf);
 
